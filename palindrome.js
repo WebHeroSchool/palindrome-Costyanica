e0b220bbb36698d152e375f8605531f8f7e02062
DESKTOP-JRC8315\79527 <raketa.souz@gmail.com>
2021-02-08 22:54:51 +0300

    test1

--- palindrome.js
+++ palindrome.js
@@ -1,5 +1,14 @@
 function palindrome(str) {
-  return null;
+
+  var lengthString = str.length;
+  var middleString = lengthString/2;
+
+  for ( var i = 0; i < middleString; i++) {
+    if ( str[i] !== str[lengthString - 1 -i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = palindrome;
